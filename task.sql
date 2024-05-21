63d0cb31f2a1ee3cdede5cfe6b244bc704318a15
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2024-05-21 19:26:47 +0300

    chore: add sql script to create db with related tables

--- task.sql
+++ task.sql
@@ -1,1 +1,34 @@
-# Write your SQL code for the database creation here. Good luck! 
+CREATE DATABASE ShopDB;
+
+USE ShopDB;
+
+CREATE TABLE Products (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(50),
+  Description VARCHAR(100),
+  Price INT,
+  WarehouseAmount INT
+);
+
+CREATE TABLE Customers (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  FirstName VARCHAR(50),
+  LastName VARCHAR(50),
+  Email VARCHAR(50),
+  Address VARCHAR(100)
+);
+
+CREATE TABLE Orders (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  CustomerID INT,
+  FOREIGN KEY(CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL,
+  Date DATE
+);
+
+CREATE TABLE OrderItems (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  OrderID INT,
+  FOREIGN KEY(OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+  ProductID INT,
+  FOREIGN KEY(ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+);
